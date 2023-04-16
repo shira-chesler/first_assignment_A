dd21229f7334373676659f193f67012e4547b4d4
shira-chesler <98814442+shira-chesler@users.noreply.github.com>
2023-04-16 19:18:35 +0300

    Update AdptArray.c

--- AdptArray.c
+++ AdptArray.c
@@ -12,14 +12,19 @@ typedef struct AdptArray_
     PRINT_FUNC print_function;
 }AdptArray;
 
+/// @brief creating an AdptArray
+/// @param copy copy function for the elements inside adptArry
+/// @param delete delete function for the elements inside adptArry 
+/// @param print print function for the elements inside adptArry
+/// @return pointer to the new adptArray
 PAdptArray CreateAdptArray(COPY_FUNC copy, DEL_FUNC delete,PRINT_FUNC print)
 {
-    if (copy==NULL || delete==NULL || print==NULL)
+    if (copy==NULL || delete==NULL || print==NULL) //one of the function's null
     {
         return NULL;
     }
     PAdptArray paa = (PAdptArray) malloc(sizeof(AdptArray));
-    if (!paa) 
+    if (!paa) //allocation failed
     {
         return NULL;
     }
@@ -31,13 +36,16 @@ PAdptArray CreateAdptArray(COPY_FUNC copy, DEL_FUNC delete,PRINT_FUNC print)
     paa->print_function = print;
     return paa;
 }
+
+/// @brief deletes the AdptArray
+/// @param paa pointer to the AdptArray
 void DeleteAdptArray(PAdptArray paa)
 {
-    if(!paa)
+    if(!paa) //pointer null
     {
         return;
     }
-    if(!(paa->pElemArr))
+    if(!(paa->pElemArr)) //array doesn't exists
     {
         free(paa);
         return;
@@ -56,14 +64,21 @@ void DeleteAdptArray(PAdptArray paa)
 
 //at this function (setadptarray) I used my solution for question 4 in the Moed A in software systems 1 test
 //also, I used the given code in the test for the parts that I didn't had to write on my own there as basis for this function
+/// @brief set AdptArray at given index
+/// @param paa pointer to AdptArray
+/// @param idx index to set AdptArray at
+/// @param p_new_e pointer to the new element to put at the AdptArray
+/// @return if seting the new element at the AdptArray succeded or not
 Result SetAdptArrayAt(PAdptArray paa, int idx, PElement p_new_e)
 {
     if (paa == NULL || idx < 0)
     {
         return FAIL;
     }
+    
     int arr_size = paa->ArrSize;
-    if (idx >= arr_size)
+    
+    if (idx >= arr_size) //needs to allocate new memory
     {
         PElement* new_arr = (PElement*) calloc((idx + 1), sizeof(PElement));
         if (!new_arr)
@@ -74,23 +89,28 @@ Result SetAdptArrayAt(PAdptArray paa, int idx, PElement p_new_e)
         {
             new_arr[i] = paa->pElemArr[i];
         }
-        free(paa->pElemArr);
+        free(paa->pElemArr); //frees old AdptArray container
         paa->pElemArr = new_arr;
     }
     if(paa->pElemArr[idx])
     {
-        paa->delete_function(paa->pElemArr[idx]);
+        paa->delete_function(paa->pElemArr[idx]); //replacing element (?)
     }
-    paa->pElemArr[idx] = paa->copy_function(p_new_e);
-    if(idx>=arr_size)
+    paa->pElemArr[idx] = paa->copy_function(p_new_e);//placing element (!)
+    if(idx>=arr_size) //needs to change container size
     {
         paa->ArrSize=idx+1;
     }
     return SUCCESS;
 }
+
+/// @brief get AdptArray element at specific index
+/// @param paa pointer to the AdptArray
+/// @param idx index to get from at array
+/// @return the element at requested index
 PElement GetAdptArrayAt(PAdptArray paa, int idx)
 {
-    if(paa==NULL || paa->pElemArr==NULL || idx>paa->ArrSize-1 || idx < 0)
+    if(paa==NULL || paa->pElemArr==NULL || idx>paa->ArrSize-1 || idx < 0) //request is not proper
     {
         return NULL;
     } 
@@ -106,6 +126,10 @@ PElement GetAdptArrayAt(PAdptArray paa, int idx)
         }
     }
 }
+
+/// @brief gets the container AdptArray size
+/// @param paa pointer to the AdptArray
+/// @return the size of the AdptArray
 int GetAdptArraySize(PAdptArray paa)
 {
     if(!paa)
@@ -114,6 +138,9 @@ int GetAdptArraySize(PAdptArray paa)
     }
     return paa->ArrSize;
 }
+
+/// @brief prints all the objects inside the DB
+/// @param paa pointer to the AdptArray
 void PrintDB(PAdptArray paa)
 {
     if(paa==NULL || paa->pElemArr==NULL)
